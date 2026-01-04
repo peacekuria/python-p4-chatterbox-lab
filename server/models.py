b7e7551ecab7b06fce1aec03955c84ad42120a65
Ephy <macbookpro@Ephys-MacBookPro.local>
2026-01-05 02:21:38 +0300

    debug

--- server/models.py
+++ server/models.py
@@ -12,3 +12,7 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now())
